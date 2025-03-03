85ac24e7d29fb4b14161559c2b97f39ea20c1bfd
zerg959 <zerg959@yandex.ru>
2025-03-03 16:18:21 +0300

    orders: methods and tests

--- cafe_main/conftest.py
+++ cafe_main/conftest.py
@@ -1,2 +1,25 @@
 import pytest
-from django.contrib.auth.models import User
+from django.conf import settings
+from orders.models import Order
+# from django.contrib.auth.models import User
+import os
+
+@pytest.fixture(scope="session")  # autouse=True означает, что она будет автоматически использоваться для всех тестов
+def use_in_memory_db():
+    settings.DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+
+@pytest.fixture
+def dishes_for_test():
+    dishes_for_test = [{'name': 'Pizza', 'price': 12.50}, {'name': 'Pasta', 'price': 10.00}]
+    return dishes_for_test
+
+@pytest.fixture
+def order_for_test():
+    dishes = [{'name': 'Pizza', 'price': 12.50}, {'name': 'Pasta', 'price': 10.00}]
+    order = Order.create_order(
+        table_number=2,
+        dishes=dishes)
+    return order
--- cafe_main/orders/models.py
+++ cafe_main/orders/models.py
@@ -1,9 +1,8 @@
 # https://ru.hexlet.io/courses/python-django-orm/lessons/annotation/theory_unit 
 
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Tuple
 from django.db import models
 from django.core.exceptions import ValidationError
-
 
 class Order(models.Model):
     """
@@ -17,7 +16,7 @@ class Order(models.Model):
     - count_total_price: calculate total price of all dishes in order;
     - save: record new data in object attr.
     """
-    STATUSES_ORDERS = (
+    STATUSES_ORDERS: Tuple = (
         ("cooking", "Cooking"),
         ("ready", "Ready"),
         ("paid", "Paid"),
@@ -25,33 +24,64 @@ class Order(models.Model):
     table_number: int = models.PositiveIntegerField(
         verbose_name="table number"
         )
-    items: List[dict] = models.JSONField(
+    dishes: List[dict] = models.JSONField(
         verbose_name="list of dishes",
         default=[]
     )
     total_price: float = models.DecimalField(
         decimal_places=2,
+        max_digits=10,
         verbose_name="total price",
+        default=0,
         )
     status_order: str = models.CharField(
+        max_length=20,
         verbose_name="order status",
         choices=STATUSES_ORDERS,
-        default="Cooking"
+        default="cooking"
     )
+
+    def change_status(self, new_status):
+        # statuses = STATUSES_ORDERS
+        if new_status != self.status_order:
+            if new_status not in dict(self.STATUSES_ORDERS).keys():
+                raise ValidationError('Unknown status')
+            self.status_order = new_status
+            self.save()
+            return True
+        return False
+        
+        
+
 
     def count_total_price(self) -> None:
         """
         Count total price of the order.
         """
-        if not self.items:
+        if not self.dishes:
             raise ValidationError('No dishes in order!')
 
-        self.total_price = sum(item["price"] for item in self.items)
-        self.save()
+        self.total_price = sum(dish["price"] for dish in self.dishes)
 
     def save(self, *args, **kwargs) -> None:
+        """
+        Save Order-object in DB.
+        """
         super().save(*args, **kwargs)
-        self.count_total_price()
+
+    @classmethod
+    def create_order(cls, table_number, dishes):
+        """
+        Create Order object in DB.
+        """
+        new_order = cls(
+            table_number=table_number,
+            dishes=dishes,
+            )
+        new_order.count_total_price()
+        new_order.save()
+        return new_order
+
 
     def __str__(self) -> str:
-        return f'Order {self.id}, Table: {self.table_number}'
+        return f'Order {self.id}, Table: {self.table_number}, Total price: {self.total_price}'
