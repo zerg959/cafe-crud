fb46968735a1bbe23c4f676119a67f7140acad56
zerg959 <zerg959@yandex.ru>
2025-03-04 17:13:17 +0300

    MVP without tests

--- cafe_main/cafe_main/settings.py
+++ cafe_main/cafe_main/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'cafe_main/static'),  # Путь к вашей папке static
+    os.path.join(BASE_DIR, 'cafe_main/static'),  # Path to static
 ]
 
 # Default primary key field type
